6a818e16ed96f69b69d6e0c2f1ce38af7225eea4
batuh <batuh@BATU-PC>
2023-06-20 20:04:19 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿//10 elamanlı bir tamsayı diziyi klavyeden
+ // doldurup eleman toplamı
+ // ve ortalamasını yazdırın
+
+int toplam= 0;
+int[] dizi = new int[10];
+for (int i = 0; i < dizi.Length; i++)
+{
+    //Console.Write(i + 1 + ". sayıyı gir: ");
+    Console.Write("{0}. sayıyı gir:", i + 1);
+
+    dizi[i] = int.Parse(Console.ReadLine());
+    toplam += dizi[i];
+}
+Console.WriteLine("Eleman Toplamı=" + toplam);
+Console.WriteLine("Dizi Ortalaması=" + (double)toplam / dizi.Length);
+
+Console.ReadKey();
